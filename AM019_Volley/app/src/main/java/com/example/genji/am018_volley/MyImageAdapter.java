cc4189fe48700a4197b86c42aa91c0ba79aaf48c
Andrea Morettin <divino.marchese@gmail.com>
2016-02-15 14:26:48 +0100

    AM019_Volley finished

--- AM019_Volley/app/src/main/java/com/example/genji/am018_volley/MyImageAdapter.java
+++ AM019_Volley/app/src/main/java/com/example/genji/am018_volley/MyImageAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 
 import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.NetworkImageView;
@@ -16,6 +17,11 @@ public class MyImageAdapter extends BaseAdapter {
 
     private Context mContext;
     private ImageLoader mImageLoader;
+
+    // ViewHolder pattern
+    static class ViewHolder {
+        ImageView imageView;
+    }
 
     public MyImageAdapter(Context c, ImageLoader i) {
         mContext = c;
@@ -40,22 +46,28 @@ public class MyImageAdapter extends BaseAdapter {
 
     // create a new NetworkImageView for each item referenced by the Adapter
     public View getView(int position, View convertView, ViewGroup parent) {
-        LayoutInflater inflater = (LayoutInflater) mContext
-                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View itemView;
-        if (convertView == null) {
+
+        View itemView = convertView;
+        final ViewHolder viewHolder;
+        if (itemView == null) {
+            LayoutInflater inflater = (LayoutInflater) mContext
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             // if it's not recycled, initialize some attributes
             itemView = inflater.inflate(R.layout.item, null);
+            viewHolder = new ViewHolder();
+            viewHolder.imageView = (ImageView)itemView.findViewById(R.id.networkImageView);
+            itemView.setTag(viewHolder);
             // set image based on selected text
             NetworkImageView nImageView = (NetworkImageView) itemView.findViewById(R.id.networkImageView);
-            // nImageView.setDefaultImageResId(R.drawable.no_image);
-            // nImageView.setErrorImageResId(R.drawable.error);
-            // this allows the image boundaries to be adjusted while keeping its aspect ratio
-            // nImageView.setAdjustViewBounds(true);
-            nImageView.setImageUrl(Images.URL[position], mImageLoader);
         } else {
-            itemView = convertView;
+            viewHolder = (ViewHolder)itemView.getTag();
         }
+        NetworkImageView nImageView = ( NetworkImageView) viewHolder.imageView;
+        // nImageView.setDefaultImageResId(R.drawable.no_image);
+        // nImageView.setErrorImageResId(R.drawable.error);
+        // this allows the image boundaries to be adjusted while keeping its aspect ratio
+        // nImageView.setAdjustViewBounds(true);
+        nImageView.setImageUrl(Images.URL[position], mImageLoader);
         return itemView;
     }
 }
